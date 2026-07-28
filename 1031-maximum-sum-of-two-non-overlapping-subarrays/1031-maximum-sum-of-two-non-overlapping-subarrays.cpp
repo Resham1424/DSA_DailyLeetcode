7176451faffac8f29881c48e3791059006324494
Shaik Resham <rresham317@gmail.com>
2026-07-28 21:29:37 +0530

    Time: 0 ms (100%), Space: 12.1 MB (82.03%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+     int funct(vector<int>& nums, int L, int M) {
+        int n = nums.size();
+
+        int lBlockSum = 0;
+        int mBlockSum = 0;
+
+        //starting window from index 0 to L+M-1
+        for(int i = 0; i <= L+M-1; i++) {
+            if(i < L) {
+                lBlockSum += nums[i];
+            } else {
+                mBlockSum += nums[i];
+            }
+        }
+
+        int maxLeftSubSum = lBlockSum;
+        int result = maxLeftSubSum + mBlockSum;
+
+        for(int mEnd = L + M; mEnd < n; mEnd++) {
+            lBlockSum += nums[mEnd-M] - nums[mEnd - M - L];
+            mBlockSum += nums[mEnd] - nums[mEnd - M];
+
+            maxLeftSubSum = max(maxLeftSubSum, lBlockSum);
+
+            result = max(result, maxLeftSubSum + mBlockSum);
+        }
+
+        return result;
+
+    }
+
+    int maxSumTwoNoOverlap(vector<int>& nums, int L, int M) {
+        int n = nums.size();
+
+        return max(funct(nums, L, M), funct(nums, M, L));
+    }
+};
