8993e94a142fceb5eadba08448c8d5ba6bf699e5
Shaik Resham <rresham317@gmail.com>
2026-07-29 20:32:42 +0530

    Time: 2 ms (50.77%), Space: 8.5 MB (92.81%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       if(x<0) return false;
+       long long original=x;
+       long long reversed=0;
+
+       while(x!=0){
+        int remainder=x%10;
+        reversed=reversed*10+remainder;
+        x/=10;
+       }
+       return original==reversed;
+    }
+};
